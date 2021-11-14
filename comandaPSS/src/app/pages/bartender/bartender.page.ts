5040fdaad08d09e48a40dccbb4de2ea863950685
Carlos Castro <castrocarlos313@gmail.com>
2021-11-13 23:34:05 -0300

    Arreglo de tiempo de elaboracion y restriccion de tipos

--- comandaPSS/src/app/pages/bartender/bartender.page.ts
+++ comandaPSS/src/app/pages/bartender/bartender.page.ts
@@ -17,17 +17,20 @@ export class BartenderPage implements OnInit {
   bebidasPendientes: Preparacion[];
   bebidasPreparando: Preparacion[];
 
-  constructor(public productoService: ProductoService, public preparacionService: PreparacionService, public pedidoService: PedidoService, private modalController: ModalController) {}
+  constructor(
+    public productoService: ProductoService,
+    public preparacionService: PreparacionService,
+    public pedidoService: PedidoService,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
-    this.preparacionService.getPreparaciones("pendiente", "bebida")
-    .then(
-      p => p.subscribe(data => this.bebidasPendientes = data)
-    );
-    this.preparacionService.getPreparaciones("preparando", "bebida")
-    .then(
-      p => p.subscribe(data => this.bebidasPreparando = data)
-    );
+    this.preparacionService
+      .getPreparaciones('pendiente', 'bebida')
+      .then((p) => p.subscribe((data) => (this.bebidasPendientes = data)));
+    this.preparacionService
+      .getPreparaciones('preparando', 'bebida')
+      .then((p) => p.subscribe((data) => (this.bebidasPreparando = data)));
   }
 
   async presentModal() {
@@ -35,17 +38,26 @@ export class BartenderPage implements OnInit {
       component: AltaProductoComponent,
       swipeToClose: true,
       presentingElement: await this.modalController.getTop(),
-      backdropDismiss: false
+      backdropDismiss: false,
+      componentProps: {
+        tipo: 'bebida',
+      },
     });
     return await modal.present();
   }
 
   public prepararPreparacion(preparacion: Preparacion) {
-    this.preparacionService.updatePreparacionState(preparacion.preparacionId, "preparando");
+    this.preparacionService.updatePreparacionState(
+      preparacion.preparacionId,
+      'preparando'
+    );
   }
 
   public terminarPreparacion(preparacion: Preparacion) {
-    this.preparacionService.updatePreparacionState(preparacion.preparacionId, "terminado");
+    this.preparacionService.updatePreparacionState(
+      preparacion.preparacionId,
+      'terminado'
+    );
   }
 
   // public async debugCrearPedido() {
@@ -55,7 +67,7 @@ export class BartenderPage implements OnInit {
   //     preparacionId: "345345",
   //     producto: await this.productoService.getById("MtUApEVwyCbirtYarvYO").then(val => val)
   //   };
-    
+
   //   let pedido: Pedido = {
   //     estado: "preparando",
   //     pedidoId: "09876",
@@ -64,7 +76,7 @@ export class BartenderPage implements OnInit {
   //     mesa: null,
   //     preparaciones: []
   //   };
-    
+
   //   pedido.preparaciones.push(p);
   //   this.pedidoService.crearPedido(pedido);
   // }
